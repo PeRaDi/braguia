13cf85dbc2d9de154f0aa26e5c17059cacc66257
pg54469 <pg54469@uminho.pt>
2024-04-26 20:12:38 +0100

    refactor

--- braguia/src/main/java/pt/uminho/braguia/App.java
+++ braguia/src/main/java/pt/uminho/braguia/App.java
@@ -2,8 +2,6 @@ package pt.uminho.braguia;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
