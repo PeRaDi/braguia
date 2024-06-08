5b59002e91989b25d25c9e0201c1222101731609
pg54469 <pg54469@uminho.pt>
2024-06-08 19:25:49 +0100

    config path aliases

--- cross-react-native/babel.config.js
+++ cross-react-native/babel.config.js
@@ -1,3 +1,31 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+          '.ios.js',
+          '.android.js',
+          '.ios.jsx',
+          '.android.jsx',
+        ],
+        root: ['.'],
+        alias: {
+          '@navigation': './src/navigation',
+          '@pins': './src/pins',
+          '@settings': './src/settings',
+          '@trails': './src/trails',
+          '@store': './src/store',
+          '@shared': './src/shared',
+          '@src': './src',
+        },
+      },
+    ],
+  ],
 };
