3a65a9d0d239f7c8fa28193c1a41df83801a970a
Pedro Braga <peradi2001@gmail.com>
2024-06-15 17:40:19 +0100

    gallery: started save to device. item selection done

--- cross-react-native/src/gallery/GalleryComponent.tsx
+++ cross-react-native/src/gallery/GalleryComponent.tsx
@@ -8,16 +8,22 @@ import {
   Modal,
   RefreshControl,
   StyleSheet,
-  TouchableHighlight,
-  View,
   Text,
+  View,
 } from 'react-native';
 import {FlatList, Image} from 'react-native';
 import Video from 'react-native-video';
 import {mediaDAO} from './MediaDAO';
 import {useSelector} from 'react-redux';
 import {selectAuth} from '@store/store';
-import {Card, IconButton, Button} from 'react-native-paper';
+import {
+  Card,
+  IconButton,
+  Button,
+  TouchableRipple,
+  Snackbar,
+  Divider,
+} from 'react-native-paper';
 
 const styles = StyleSheet.create({
   container: {
@@ -55,22 +61,37 @@ const styles = StyleSheet.create({
   closeButton: {
     marginTop: 20,
   },
-  imagePlayer: {
+  player: {
     width: '100%',
     height: '90%',
     resizeMode: 'contain',
   },
-  videoPlayer: {
+  selectionBar: {
+    position: 'absolute',
+    bottom: 0,
     width: '100%',
-    height: '90%',
-  },
-  audioPlayer: {
-    width: '100%',
-    height: '90%',
+    padding: 10,
+    backgroundColor: '#f4edf7',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    display: 'flex',
+    alignItems: 'center',
   },
 });
 
-const MediaPreview = ({media}: {media: Media}) => {
+const MediaPreview = ({
+  media,
+  onLongPress,
+  isSelected,
+  toggleSelection,
+  selectionMode,
+}: {
+  media: Media;
+  onLongPress: () => void;
+  isSelected: boolean;
+  toggleSelection: () => void;
+  selectionMode: boolean;
+}) => {
   const {isPremium} = useSelector(selectAuth);
   const [modalVisible, setModalVisible] = useState(false);
 
@@ -87,59 +108,53 @@ const MediaPreview = ({media}: {media: Media}) => {
   };
 
   const handlePress = () => {
-    setModalVisible(true);
+    if (!selectionMode) {
+      setModalVisible(true);
+    } else {
+      toggleSelection();
+    }
   };
 
   const renderModalContent = () => {
     if (media.isImage) {
-      return <Image source={{uri: media.fileUrl}} style={styles.imagePlayer} />;
+      return <Image source={{uri: media.fileUrl}} style={styles.player} />;
     }
     if (media.isVideo) {
       return (
-        <Video
-          source={{uri: media.fileUrl}}
-          style={styles.videoPlayer}
-          controls
-        />
+        <Video source={{uri: media.fileUrl}} style={styles.player} controls />
       );
     }
     if (media.isRecord) {
       return (
-        <Video
-          source={{uri: media.fileUrl}}
-          style={styles.audioPlayer}
-          controls
-        />
+        <Video source={{uri: media.fileUrl}} style={styles.player} controls />
       );
     }
     return null;
   };
 
   return (
-    <>
-      <TouchableHighlight onPress={handlePress}>
-        <View>{renderIcon()}</View>
-      </TouchableHighlight>
+    <View>
+      <TouchableRipple onPress={handlePress} onLongPress={onLongPress}>
+        <View style={{opacity: isSelected ? 0.5 : 1}}>{renderIcon()}</View>
+      </TouchableRipple>
       <Modal
         visible={modalVisible}
         transparent={false}
         animationType="slide"
         onRequestClose={() => {
           setModalVisible(false);
-        }}
-      >
+        }}>
         <View style={styles.modalContent}>
           {renderModalContent()}
           <Button
             mode="contained"
             onPress={() => setModalVisible(false)}
-            style={styles.closeButton}
-          >
+            style={styles.closeButton}>
             Close
           </Button>
         </View>
       </Modal>
-    </>
+    </View>
   );
 };
 
@@ -155,6 +170,8 @@ const GalleryComponent = ({
   navigation: any;
 }) => {
   const [refreshing, setRefreshing] = useState(false);
+  const [selectionMode, setSelectionMode] = useState(false);
+  const [selectedItems, setSelectedItems] = useState<Set<string>>(new Set());
 
   useEffect(() => {
     mediaDAO.fetchList({fromCache: true});
@@ -166,14 +183,46 @@ const GalleryComponent = ({
     setRefreshing(false);
   }, []);
 
+  const handleLongPress = useCallback((item: Media) => {
+    setSelectionMode(true);
+    setSelectedItems(prev => new Set(prev).add(item.id));
+  }, []);
+
+  const toggleSelection = useCallback((item: Media) => {
+    setSelectedItems(prev => {
+      const newSet = new Set(prev);
+      if (newSet.has(item.id)) {
+        newSet.delete(item.id);
+      } else {
+        newSet.add(item.id);
+      }
+      if (newSet.size === 0) {
+        setSelectionMode(false);
+      }
+      return newSet;
+    });
+  }, []);
+
+  const handleSaveToDevice = () => {
+    // Your logic to save selected items to the device
+    //Snackbar.show({text: 'Items saved to device!'});
+  };
+
   const renderItem: ListRenderItem<Media> = ({item}) => {
     const screenWidth = Dimensions.get('window').width;
     const itemSize = screenWidth / 3 - 10;
+    const isSelected = selectedItems.has(item.id);
 
     return (
       <View style={{width: itemSize, height: itemSize, padding: 2}}>
         <Card key={item.id} style={styles.card}>
-          <EnhancedMediaPreview media={item} />
+          <EnhancedMediaPreview
+            media={item}
+            onLongPress={() => handleLongPress(item)}
+            isSelected={isSelected}
+            toggleSelection={() => toggleSelection(item)}
+            selectionMode={selectionMode}
+          />
         </Card>
       </View>
     );
@@ -193,6 +242,18 @@ const GalleryComponent = ({
           paddingBottom: 10,
         }}
       />
+      {selectionMode && (
+        <>
+          <Divider />
+          <View style={styles.selectionBar}>
+            <Text>Itens selecionados: {selectedItems.size}</Text>
+            <Button mode="contained" onPress={handleSaveToDevice}>
+              Guardar no dispositivo
+            </Button>
+          </View>
+          <Divider />
+        </>
+      )}
     </View>
   );
 };
